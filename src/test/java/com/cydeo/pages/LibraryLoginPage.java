5053e473a9822f081250c4617839811a45d63b77
Rabia <rabiasahinuk@gmail.com>
2022-06-10 16:13:17 +0100

    day 12: LibraryLoginPage

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -34,7 +34,7 @@ public class LibraryLoginPage {
     @FindBy(xpath = "//button[.='Sign in']")
     public WebElement signInButton;
 
-    @FindBy(xpath = "//div[.='This field is required.']/div")
+    @FindBy(xpath = "//div[.='This field is required.']/div") // 2.div goes the child
     public WebElement fieldRequiredErrorMessage;
 
     @FindBy(xpath = "//div[.='Please enter a valid email address.']/div")
